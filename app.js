919654d728acd9f7478ade005cde16fc69881a7f
Andron <roniosipov7@gmail.com>
2022-01-23 16:18:34 +0400

    Update #1

---
+++ app.js
@@ -0,0 +1,12 @@
+const ul = document.querySelector(".con .mainUl");
+
+let i = 1;
+
+function bruh() {
+    var inputValue = document.getElementById("mainInput").value;
+    var li = document.createElement("LI");
+    var text1 = document.createTextNode(i + ". " + inputValue);
+    li.appendChild(text1);
+    ul.appendChild(li);
+    i++;
+}
