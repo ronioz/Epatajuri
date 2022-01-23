129dc6870bdaddb6effaa81349e04621a980b5d5
Kr1ner <krineri12@gmail.com>
2022-01-23 16:35:54 +0400

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const ul = document.querySelector(".con .mainUl");
 
 let i = 1;
 
+let database = [];
+
 function bruh() {
     var inputValue = document.getElementById("mainInput").value;
     var li = document.createElement("LI");
